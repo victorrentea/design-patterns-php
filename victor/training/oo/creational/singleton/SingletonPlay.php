d4bb92008ccaa964b4372411819770370892326d
Victor Rentea <victorrentea@gmail.com>
2021-01-18 14:56:55 +0200

    end of day 1

--- victor/training/oo/creational/singleton/SingletonPlay.php
+++ victor/training/oo/creational/singleton/SingletonPlay.php
@@ -10,6 +10,23 @@ namespace victor\training\oo\behavioral\singleton;
 include('AppConfiguration.php');
 
 
+class CodDeBiz
+{
+    private AppConfiguration $appConfig;
+
+    public function __construct(AppConfiguration $appConfig)
+    {
+        $this->appConfig = $appConfig;
+    }
+
+    function m()
+    {
+        if ($this->appConfig->getProperty('a') === '') {
+            throw new \Exception();
+        }
+    }
+}
+
 printf('Configuration setting a = ' . (AppConfiguration::getInstance())->getProperty('a') . "\n");
 printf('Configuration setting b = ' . (AppConfiguration::getInstance())->getProperty('b'). "\n");
 printf('Configuration setting a = ' . (AppConfiguration::getInstance())->getProperty('a') . "\n");
