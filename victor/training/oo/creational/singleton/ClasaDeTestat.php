2eda2467cdf0c1997783c3808327aeae37ebdbe0
Victor Rentea <victorrentea@gmail.com>
2021-01-18 10:34:44 +0200

    ServiceLocator PATTERN - in loc sa faci NEW folosesti o metoda statica pentru a obtine dependentele necesare -->> permite lifecycle management (sa-i dai mereu aceeasi instanta) + MOCKing din teste!

---
+++ victor/training/oo/creational/singleton/ClasaDeTestat.php
@@ -0,0 +1,64 @@
+
+<?php
+
+
+namespace victor\training\oo\creational\singleton;
+
+
+class ClasaDeTestat
+{
+//    /** @var B b */
+//    private $b;
+//
+//    public function __construct(B $b)
+//    {
+//        $this->b = $b;
+//    }
+
+    public function metDeBiz() {
+        // logica de aici
+//        (new B())->met(); // 15 in urma
+
+        /** @var B $b */
+        $b = ServiceLocator::getObject(B::class);
+        $b->met();
+
+//        $this->b->met(); // acum
+    }
+}
+
+class ServiceLocator {
+
+    static private $registry = [];
+    static public $mockedClassesFromTESTS = []; // in productie ramane gol
+
+    public static function getObject(string $class)
+    {
+        if (isset($class, self::$mockedClassesFromTESTS)) {
+            return self::$mockedClassesFromTESTS[$class];
+        }
+        if (isset($class, self::$registry)) {
+            return self::$registry[$class];
+        }
+
+        $newInstance = new $class;
+        self::$registry[$class] = $newInstance;
+        return $newInstance;
+    }
+}
+
+class Testul {
+    /** @test */
+    public function test1() {
+        ServiceLocator::$mockedClassesFromTESTS["\victor\training\oo\creational\singleton\B"] = mock(B::class);
+    }
+}
+
+
+class B {
+
+    public function met()
+    {
+        // ceva cu fisiere/ DB/ REST/ gherkin/rabbit
+    }
+}
