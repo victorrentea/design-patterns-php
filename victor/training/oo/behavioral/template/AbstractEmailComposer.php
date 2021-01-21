620849229b8ccad38f11e998edfb9a77c7d4f46e
Victor Rentea <victorrentea@gmail.com>
2021-01-21 11:26:39 +0200

    EmailSender just became SINGLEton - . pentru ca nu mai are stare specifica requestului curent.

--- victor/training/oo/behavioral/template/AbstractEmailComposer.php
+++ victor/training/oo/behavioral/template/AbstractEmailComposer.php
@@ -13,60 +13,51 @@ use phpDocumentor\Reflection\Types\Callable_;
 include "Email.php";
 include "EmailContext.php";
 
-// new EmailSender()->send(orderEmail)
 
-// (new OrderReceivedEmailSender())->sendEmail('a@b.com');
-// (new OrderShippedEmailSender())->sendEmail('a@b.com');
-
-class EmailSender
-{
+class EmailSender {
     private const MAX_RETRIES = 3;
-    private EmailSenderInterface $composer;
-    private EmailClient $client;
-
-    public function __construct(EmailSenderInterface $composer)
-    {
-        $this->composer = $composer;
-    }
-
-    public function sendEmail(string $emailAddress): void
-    {
+    public function sendEmail(string $emailAddress, EmailComposerInterface $composer): void {
         $context = new EmailContext(/*smtpConfig,etc*/);
         for ($i = 0; $i < self::MAX_RETRIES; $i++) {
             $email = new Email();
             $email->setSender('noreply@emag.ro');
             $email->setReplyTo('/dev/null');
             $email->setTo($emailAddress);
-            $this->composer->compose($email);
+            $composer->compose($email);
             $success = $context->send($email);
             if ($success) break;
         }
         // persit in db
     }
 }
-
-interface EmailSenderInterface {
+interface EmailComposerInterface {
     function compose(Email $email): void;
 }
-class OrderReceivedEmailSender implements  EmailSenderInterface {
-    public function compose(Email $email): void
-    {
+class OrderReceivedEmailComposer implements  EmailComposerInterface {
+    public function compose(Email $email): void {
         $email->setSubject('Order Received');
         $email->setBody('Thank you for your order');
     }
 }
-
-
-class OrderShippedEmailSender implements  EmailSenderInterface {
-    public function compose(Email $email): void
-    {
+class OrderShippedEmailComposer implements  EmailComposerInterface {
+    public function compose(Email $email): void {
         $email->setSubject('Order Shipped');
         $email->setBody('Ti-am trimis. Speram sa ajunga (de data asta).');
     }
 }
 
+// new EmailSender()->send(orderEmail)
+
+
 //Evolutie: implement sendOrderShippedEmail, la fel cum am trimis si la order received.
 
 
-(new EmailSender(new OrderReceivedEmailSender()))->sendEmail("a@b.com");
-(new EmailSender(new OrderShippedEmailSender()))->sendEmail("a@b.com");
+$sender = new EmailSender();
+($sender)->sendEmail("a@b.com", new OrderReceivedEmailComposer());
+($sender)->sendEmail("a@b.com", new OrderShippedEmailComposer());
+
+
+// class CodClient {
+//     // #inject
+//     private EmailSender $emailSender;
+// }
