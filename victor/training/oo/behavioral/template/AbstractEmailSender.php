264a669f2b9ecc7ae690c4d16247512d6bcc1cd5
Victor Rentea <victorrentea@gmail.com>
2021-01-21 11:11:19 +0200

    Composing a Strategy

--- victor/training/oo/behavioral/template/AbstractEmailSender.php
+++ victor/training/oo/behavioral/template/AbstractEmailSender.php
@@ -13,9 +13,20 @@ use phpDocumentor\Reflection\Types\Callable_;
 include "Email.php";
 include "EmailContext.php";
 
-abstract class AbstractEmailSender
+// new EmailSender()->send(orderEmail)
+
+// (new OrderReceivedEmailSender())->sendEmail('a@b.com');
+// (new OrderShippedEmailSender())->sendEmail('a@b.com');
+
+class EmailSender
 {
     private const MAX_RETRIES = 3;
+    private EmailSenderInterface $composer;
+
+    public function __construct(EmailSenderInterface $composer)
+    {
+        $this->composer = $composer;
+    }
 
     public function sendEmail(string $emailAddress): void
     {
@@ -25,18 +36,19 @@ abstract class AbstractEmailSender
             $email->setSender('noreply@emag.ro');
             $email->setReplyTo('/dev/null');
             $email->setTo($emailAddress);
-            $this->compose($email);
+            $this->composer->compose($email);
             $success = $context->send($email);
             if ($success) break;
         }
         // persit in db
     }
-
-    protected abstract function compose(Email $email): void;
-
 }
-class OrderReceivedEmailSender extends AbstractEmailSender {
-    protected function compose(Email $email): void
+
+interface EmailSenderInterface {
+    function compose(Email $email): void;
+}
+class OrderReceivedEmailSender implements  EmailSenderInterface {
+    public function compose(Email $email): void
     {
         $email->setSubject('Order Received');
         $email->setBody('Thank you for your order');
@@ -44,15 +56,16 @@ class OrderReceivedEmailSender extends AbstractEmailSender {
 }
 
 
-class OrderShippedEmailSender extends AbstractEmailSender {
-    protected function compose(Email $email): void
+class OrderShippedEmailSender implements  EmailSenderInterface {
+    public function compose(Email $email): void
     {
         $email->setSubject('Order Shipped');
         $email->setBody('Ti-am trimis. Speram sa ajunga (de data asta).');
     }
 }
 
-(new OrderReceivedEmailSender())->sendEmail('a@b.com');
-(new OrderShippedEmailSender())->sendEmail('a@b.com');
-
 //Evolutie: implement sendOrderShippedEmail, la fel cum am trimis si la order received.
+
+
+(new EmailSender(new OrderReceivedEmailSender()))->sendEmail("a@b.com");
+(new EmailSender(new OrderShippedEmailSender()))->sendEmail("a@b.com");
