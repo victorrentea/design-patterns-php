8d3b6a7537f02e7c5c16cd6f0bfb24d6a270d2a3
Victor Rentea <victorrentea@gmail.com>
2023-07-07 09:52:36 +0300

    Extrage intai din switch case-urile ca functii daca-s mici . KISS.

--- src/victor/training/patterns/behavioral/strategy/CustomsService.php
+++ src/victor/training/patterns/behavioral/strategy/CustomsService.php
@@ -11,21 +11,43 @@ namespace victor\training\patterns\behavioral\strategy;
 class CustomsService
 {
 
-    public function computeAddedCustomsTax(string $originCountry, float $tobaccoValue, float $otherValue): float { // UGLY API we CANNOT change
+    public function computeAddedCustomsTax(string $originCountry,
+                                           float $tobaccoValue,
+                                           float $otherValue): float { // UGLY API we CANNOT change
         switch ($originCountry) {
-            case 'UK': return $tobaccoValue/2 + $otherValue/2;
-            case 'CH': return $tobaccoValue + $otherValue;
+            case 'UK': return $this->computeUKTax($tobaccoValue, $otherValue);
+            case 'CN': return $this->computeChinaTax($tobaccoValue, $otherValue);
             case 'FR':
             case 'ES': // other EU country codes...
-            case 'RO': return $tobaccoValue/3;
+            case 'RO': return $this->computeEUTax($tobaccoValue);
             default: throw new \RuntimeException("Not a valid country ISO2 code: {$originCountry}");
         }
+    }
+
+    public function computeUKTax(float $tobaccoValue, float $otherValue): float
+    {
+        // chestii
+        // logic
+        // mai multalogic
+        // mai multa logic
+        // mai multa logic
+        return $tobaccoValue / 2 + $otherValue / 2;
+    }
+
+    public function computeChinaTax(float $tobaccoValue, float $otherValue): float
+    {
+        return $tobaccoValue + $otherValue;
+    }
+
+    public function computeEUTax(float $tobaccoValue): float
+    {
+        return $tobaccoValue / 3;
     }
 
 }
 
 
 $customsService = new CustomsService();
-printf('Tax for (RO,100,100) = ' . $customsService->computeAddedCustomsTax("RO", 100, 100) . '\n');
-printf('Tax for (CH,100,100) = ' . $customsService->computeAddedCustomsTax("CH", 100, 100) . '\n');
-printf('Tax for (UK,100,100) = ' . $customsService->computeAddedCustomsTax("UK", 100, 100) . '\n');
+printf('Tax for (RO,100,100) = ' . $customsService->computeAddedCustomsTax("RO", 100, 100) . "\n");
+printf('Tax for (CN,100,100) = ' . $customsService->computeAddedCustomsTax("CN", 100, 100) . "\n");
+printf('Tax for (UK,100,100) = ' . $customsService->computeAddedCustomsTax("UK", 100, 100) . "\n");
